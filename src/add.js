05175fb5887f50753890b1fb39558780c1295f52
zaki-yama <shingoyamazaki00@gmail.com>
2016-04-20 11:30:21 +0900

    es6でテスト書けるように

---
+++ src/add.js
@@ -0,0 +1,8 @@
+"use strict";
+const assert = require("assert");
+
+export default function add(x, y) {
+  assert(typeof x === "number");
+  assert(typeof y === "number");
+  return x + y;
+}
--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-//var assert = require('chai').assert;
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      //assert.equal(-1, [1,2,3].indexOf(5));
-      //assert.equal(-1, [1,2,3].indexOf(0));
-    });
+// require("power-assert") じゃなくて assert でもいい
+const assert = require("assert");
+import add from "../src/add";
+
+describe("add", function () {
+  it("should return x + y", function () {
+    const result = add(1, 2);
+    assert.equal(result, 5);// <= Wrong
   });
 });
